717f5938508ecf7d7aef6e522919051335828c7f
luiggy-mamani-jalafund <luiggy.mamani.12777190@usalesiana.edu.bo>
2024-11-09 10:18:14 -0400

    feat: added put endpoint to vote for an specific poll option

--- api/src/main/java/luiggy/test/api/controllers/PollController.java
+++ api/src/main/java/luiggy/test/api/controllers/PollController.java
@@ -2,20 +2,25 @@ package luiggy.test.api.controllers;
 
 import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
+import luiggy.test.api.application.use_cases.IPollOptionService;
 import luiggy.test.api.application.use_cases.IPollService;
 import luiggy.test.api.infrastructure.dtos.requests.CreatePollDto;
+import luiggy.test.api.infrastructure.dtos.requests.VoteOptionDto;
 import luiggy.test.api.infrastructure.dtos.responses.FetchedPoll;
+import luiggy.test.api.infrastructure.dtos.responses.VoteResponseDto;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/api/v1/polls")
 @AllArgsConstructor
 public class PollController {
 
-    private IPollService pollService;
+    private final IPollService pollService;
+    private final IPollOptionService pollOptionService;
 
     @PostMapping
     public ResponseEntity<FetchedPoll> createPoll(@RequestBody @Valid CreatePollDto createPollDto) {
@@ -25,6 +30,11 @@ public class PollController {
     @GetMapping
     public ResponseEntity<List<FetchedPoll>> getAll() {
         return ResponseEntity.ok(pollService.getAll());
+    }
+
+    @PutMapping("{id}/votes")
+    public ResponseEntity<VoteResponseDto> vote(@PathVariable("id") UUID pollId, @RequestBody VoteOptionDto voteOptionDto) {
+        return ResponseEntity.ok(pollOptionService.vote(pollId, voteOptionDto));
     }
 
 
