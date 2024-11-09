185f4eee9af0e4af907bf33d39d15e087c67302c
luiggy-mamani-jalafund <luiggy.mamani.12777190@usalesiana.edu.bo>
2024-11-09 11:59:06 -0400

    ci: added poll request to fetch polls

--- frontend/src/utils/requesters/PollRequester.ts
+++ frontend/src/utils/requesters/PollRequester.ts
@@ -1,4 +1,12 @@
 import { CreatePollDto } from "@/models/dtos/CreatePollDto";
+import { FetchedPoll } from "@/models/dtos/FetchedPollDto";
+
+export async function gellAllPolls(): Promise<FetchedPoll[]> {
+    const rest = await fetch("http://localhost:8080/api/v1/polls");
+    const polls = await rest.json();
+
+    return polls;
+}
 
 export async function createPoll(poll: CreatePollDto) {
     return await fetch("http://localhost:8080/api/v1/polls", {
