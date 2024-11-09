421290b86adfb163c07b3b65c580af04e4ac519c
luiggy-mamani-jalafund <luiggy.mamani.12777190@usalesiana.edu.bo>
2024-11-09 10:16:16 -0400

    ci: added signature method to vote for a specific poll option in the service and repository

--- api/src/main/java/luiggy/test/api/application/use_cases/IPollOptionService.java
+++ api/src/main/java/luiggy/test/api/application/use_cases/IPollOptionService.java
@@ -1,11 +1,15 @@
 package luiggy.test.api.application.use_cases;
 
+import luiggy.test.api.infrastructure.dtos.requests.VoteOptionDto;
 import luiggy.test.api.infrastructure.dtos.responses.FetchedPollOption;
+import luiggy.test.api.infrastructure.dtos.responses.VoteResponseDto;
 
 import java.util.List;
 import java.util.UUID;
 
 public interface IPollOptionService {
+
+    VoteResponseDto vote(UUID pollId, VoteOptionDto voteOptionDto);
 
     List<FetchedPollOption> saveMany(List<String> options, UUID pollId);
 
--- api/src/main/java/luiggy/test/api/domain/repositories/IPollVoteRepository.java
+++ api/src/main/java/luiggy/test/api/domain/repositories/IPollVoteRepository.java
@@ -14,4 +14,6 @@ public interface IPollVoteRepository {
 
     int countVotesByPollOptionId(UUID pollOptionId);
 
+    PollVote save(PollVote newVote);
+
 }
