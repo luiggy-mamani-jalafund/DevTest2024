8602870e1cb83060eabb57af30898e3b5c0c8e6e
luiggy-mamani-jalafund <luiggy.mamani.12777190@usalesiana.edu.bo>
2024-11-09 11:42:39 -0400

    build: added cors security config

--- api/src/main/java/luiggy/test/api/infrastructure/config/security/CorsConfig.java
+++ api/src/main/java/luiggy/test/api/infrastructure/config/security/CorsConfig.java
@@ -10,7 +10,8 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**")
-                .allowedOrigins("http://localhost")
+                .allowedOrigins("http://localhost:3000/")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
     }
