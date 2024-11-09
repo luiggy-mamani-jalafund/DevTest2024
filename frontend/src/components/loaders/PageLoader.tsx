7d3c1287f5b76e1e3399862e610da0b974c8ab7c
luiggy-mamani-jalafund <luiggy.mamani.12777190@usalesiana.edu.bo>
2024-11-09 11:58:18 -0400

    ci: added loaders

---
+++ frontend/src/components/loaders/PageLoader.tsx
@@ -0,0 +1,11 @@
+import SmallLoader from "./SmallLoader";
+
+const PageLoader = () => {
+    return (
+        <div>
+            <SmallLoader />
+        </div>
+    );
+};
+
+export default PageLoader;
